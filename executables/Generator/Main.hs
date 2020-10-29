b1ce36079e7733c8150aed1a0d66c967681d2e25
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2020-10-28 22:42:33 -0400

    Add parse args in generator

--- executables/Generator/Main.hs
+++ executables/Generator/Main.hs
@@ -3,17 +3,44 @@ module Main where
 import HidatoLib
 import qualified System.Random as R
 
+import Data.Semigroup ((<>))
+import Options.Applicative
 
-
+data Opts = Opts
+    {  	template   :: !String,
+	board_name :: !String,
+	rotate     :: Int
+    }
 
 main :: IO ()
 main = do
-    putStrLn "Insert template name"
-    fileName <- getLine
-    putStrLn "Insert name of the board"
-    name <- getLine
+    opts <- execParser optsParser
+    putStrLn (concat ["Using ", template opts, " as selected file"])
+    putStrLn (concat ["Rotating ", show $ rotate opts, " time(s)"])
+    let fileName = template opts 
+    let name = board_name opts 
     board <- parseBoard fileName
     numberedBoard <- gameGenerator board
     putStrLn $ printBoard numberedBoard
     writeFile name $ printBoard numberedBoard
     
+optsParser :: ParserInfo Opts
+optsParser =
+    info
+        (helper <*> versionOption <*> programOptions)
+        (fullDesc <> progDesc "Hidato Generator haskell implementation" <>
+         header
+             "Hidato - Project of haskell")
+versionOption :: Parser (a -> a)
+versionOption = infoOption "Hidato Generator Version 0.1" (long "version" <> help "Show version")
+programOptions :: Parser Opts
+programOptions =
+    Opts <$> strOption
+        (long "template" <> metavar "VALUE" <> value "templates/Elipse.txt" <>
+         help "Selected template to generate Board")
+	<*> strOption
+        (long "board_name" <> metavar "VALUE" <> value "board" <>
+         help "Name of generated value")
+	<*> option auto
+        (long "rotate" <> metavar "INT" <> value 0 <>
+         help "Rotate entry 90 degrees n times")
