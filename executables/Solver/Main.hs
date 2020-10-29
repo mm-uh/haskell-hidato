02823815907ac9338e5e8000d3cdfc326efa8e20
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2020-10-28 22:42:50 -0400

    Add parse args in Solver

--- executables/Solver/Main.hs
+++ executables/Solver/Main.hs
@@ -4,15 +4,33 @@ import HidatoLib
 import qualified Data.Map as M
 import qualified Data.IntSet as S
 import Data.Maybe
+import Data.Semigroup ((<>))
+import Options.Applicative
 
-
+data Opts = Opts
+    {  file :: !String
+    }
 
 main :: IO()
 main = do
-    fn <- getLine
-    b <- parseGame fn
+    opts <- execParser optsParser
+    putStrLn (concat ["Using ", file opts, " as selected file"])
+    
+    b <- parseGame $ file opts
     let sols = solve b
     if isNothing $ sols then putStrLn "There is not solution" else putStrLn $ printBoard $ head $ fromJust $ sols
    
-
-    
+optsParser :: ParserInfo Opts
+optsParser =
+    info
+        (helper <*> versionOption <*> programOptions)
+        (fullDesc <> progDesc "Hidato haskell implementation" <>
+         header
+             "Hidato - Project of haskell")
+versionOption :: Parser (a -> a)
+versionOption = infoOption "Hidato Version 0.1" (long "version" <> help "Show version")
+programOptions :: Parser Opts
+programOptions =
+    Opts <$> strOption
+        (long "file" <> metavar "VALUE" <> value "elipse" <>
+         help "file with generated hidato")
